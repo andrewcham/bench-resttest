65ac222deca528ab6cb79e8c488e7e1d76050f40
acham <cham.andrew@gmail.com>
2015-04-21 16:01:53 -0700

    Removed useless totalBalance variable

--- resttest.py
+++ resttest.py
@@ -5,7 +5,7 @@ import math
 # Helper Functions
 ################################################################################
 def sumTransAmounts(lot):
-	return sum([float(t['Amount']) for t in lot])
+    return sum([float(t['Amount']) for t in lot])
 
 ################################################################################
 # Main Logic
@@ -23,14 +23,13 @@ stumbled across a 404 (but then we would make assumptions that empty pages
 returned 404s, and we have total number of transactions available to us anyway).
 '''
 r = requests.get(GET_transpage_stub.format(1)).json()
-total_balance = float(r['totalBalance'])
 total_count = r['totalCount']
 total_pages = int(math.ceil(total_count / MAX_NUM_TRANS))
 lot = r['transactions']
 
 for page in range(2, total_pages + 1):
-	r = requests.get(GET_transpage_stub.format(page)).json()
-	lot.extend(r['transactions'])
+    r = requests.get(GET_transpage_stub.format(page)).json()
+    lot.extend(r['transactions'])
 
-# Return 
+# Return the total transactions amount
 print sumTransAmounts(lot)
